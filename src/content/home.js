64385ba6ea4dedaf1131ea06ed4b88300984ea80
Oscar Soanes <oscarsoanes@gmail.com>
2023-03-03 18:45:44 +0000

    Main home menu is designed

--- src/content/home.js
+++ src/content/home.js
@@ -1,6 +1,8 @@
 export function setAsHome() {
     const container = document.getElementById("content")
     const mainEl = document.querySelector("main");
+    const homeContainer = document.createElement("div");
+    homeContainer.classList.add("home-container");
     const slogan = document.createElement("p");
     slogan.textContent = "Fresh fish, expertly rolled";
 
@@ -8,12 +10,14 @@ export function setAsHome() {
     date.textContent = "Established in 2023";
 
     const img = document.createElement("img");
+    img.classList.add("home-img")
     img.src = "assets/home-sushi.jpg";
     img.alt = "Sushi";
 
     const orderMsg = document.createElement("p");
     orderMsg.textContent = "Order online or visit us!"
 
-    mainEl.append(slogan, date, img, orderMsg)
+    homeContainer.append(slogan, date, img, orderMsg)
+    mainEl.appendChild(homeContainer);
     container.appendChild(mainEl);
 }
