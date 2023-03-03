fd689361ce62765d31095608e765f03caa416fb8
Oscar Soanes <oscarsoanes@gmail.com>
2023-03-03 17:59:36 +0000

    All products added to the menu via menu.js

--- src/content/menu.js
+++ src/content/menu.js
@@ -1,6 +1,12 @@
 export function setAsMenu() {
     addElementToMain("dragon-roll.jpg", "Dragon Roll Sushi", "Dragon Roll Sushi", "Panko prawn and cucumber roll with grilled eel top and Teriyaki, sweet spicy sauce and ginger.");
-    addElementToMain("cucumb-avacado-maki.jpg", "Cucumber Avacado Maki", "Cucumber Avacado Maki", "Sushi roll with avocado cucumber and sesame.")
+    addElementToMain("cucumb-avacado-maki.jpg", "Cucumber Avacado Maki", "Cucumber Avacado Maki", "Sushi roll with avocado cucumber and sesame.");
+    addElementToMain("salmon-maki.jpg", "California Maki", "California Maki", "Sushi roll with crabmeat, avocado, cucumber and tobiko.");
+    addElementToMain("salmon-sashimi.jpg", "Salmon Sashimi (Small)", "Salmon Sashimi (Small)", "7 piece Salmon sashimi");
+    addElementToMain("salmon-sashimi-xl.jpg", "Salmon Sashimi (Large)", "Salmon Sashimi (Large)", "15 piece Salmon sashimi");
+    addElementToMain("salmon-tuna-sashimi.jpg", "Salmon Tuna Sashimi", "Salmon TUna Sashimi", "4 piece salmon sashimi and 3 piece tuna sashimi");
+    addElementToMain("sushi-delight.jpg", "Salmon Delight Roll Box", "Salmon Delight Roll Box", "Red cabbage, avo cucumber roll, topped with fresh salmon, wasabi mayo, furikage and tobiko.");
+    addElementToMain("tuna-roll.jpg", "Tuna Delight Roll Box", "Tune Delight Roll Box", "Red cabbage, avo cucumber roll, topped with fresh salmon, wasabi mayo, furikage and tobiko.");
 }
 
 function addElementToMain(imgSrc, imgAlt, productName, productDescription) {
